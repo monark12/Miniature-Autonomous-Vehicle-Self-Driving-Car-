fb14c25e6d8849d6d681d4960e7863bda1f7cc77
Gautam Sharma <gautamsharma@Gautams-MacBook-Air.local>
2017-02-15 02:56:18 +0530

    untabify

--- servo.py
+++ servo.py
@@ -9,19 +9,19 @@ import time
 
 
 class Servo(object):
-	def __init__(self, channel=17, frequency=100, duty_cycle=5):
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(self.channel, GPIO.OUT)
-		self.pwm = GPIO.PWM(self.channel, self.frequency)
-		self.pwm.start(self.duty_cycle)
+  def __init__(self, channel=17, frequency=100, duty_cycle=5):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(self.channel, GPIO.OUT)
+    self.pwm = GPIO.PWM(self.channel, self.frequency)
+    self.pwm.start(self.duty_cycle)
 
-	def turn(angle):
-		self.pwm.ChangeDutyCycle(float(angle) / 10.0 + 2.5)
-		time.sleep(0.02) 
+  def turn(angle):
+    self.pwm.ChangeDutyCycle(float(angle) / 10.0 + 2.5)
+    time.sleep(0.02) 
 
 
 # def main():
-# 	pass
+#   pass
 
 # if __name__ == '__main__':
-	# main()
+  # main()
