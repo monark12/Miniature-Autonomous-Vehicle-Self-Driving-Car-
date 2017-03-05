14ea76a5a37ce95ac0390288a7ef127297c5899e
Gautam Sharma <gautamsharma7234@gmail.com>
2017-03-05 18:04:53 +0530

    client side working

--- sdrcc/selfdrive/client.py
+++ sdrcc/selfdrive/client.py
@@ -5,7 +5,6 @@ import socket
 import picamera
 import numpy as np
 import io
-#import controller 
 from util import servo
 from util import motor
 
@@ -16,16 +15,17 @@ client_socket.connect(('192.168.0.6', 8000))
 print('aft con')
 ######
 
+connection = client_socket.makefile('wb')
+
 #######recieving
 server_socket = socket.socket()
-server_socket.bind(('192.168.0.5', 8000))
+server_socket.bind(('192.168.0.5', 8001))
 server_socket.listen(0)
 #######
 
-#c = controller.Controller()
+s = servo.Servo(4)
 
-connection = client_socket.makefile('wb')
-recieve_connection = server_socket.accept()[0].makefile('rb')
+recieve_connection = server_socket.accept()[0]
 
 try:
   with picamera.PiCamera() as camera:
@@ -43,20 +43,21 @@ try:
       
       stream.seek(0)
       stream.truncate()
-      connection.write(struct.pack('<l',0))
 
 
       # receive
-      #steering_angle = float(server_socket.recv(24))
-      steering_angle = struct.unpack('d', server_socket.recv(24))[0]
-      servo.turn(steering_angle)
+      steering_angle = float(recieve_connection.recv(1024).decode("ascii"))
+      print("Received steering angle --> ", steering_angle)
+      s.turn(steering_angle)
       #motor.foward(80)
   
-
 
 except KeyboardInterrupt:
   sys.exit(-1)
 
 finally:
+  print('closing sockets')
+  s.center()
+  s.stop()
   connection.close()
   client_socket.close()
